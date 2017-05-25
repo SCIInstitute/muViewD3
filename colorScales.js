eba1f28ca8eaeb6faca279da36f6f95c042eabfd
Magdalena <a1209910@unet.univie.ac.at>
2017-05-25 11:26:58 +0200

    interactive color scale change '

---
+++ colorScales.js
@@ -0,0 +1,14 @@
+var colorScales = {};
+colorScales.redToBlueFlip = ["#BD0C0C", "#BD2918", "#BD4A29", "#FE9256", "#FEBF79", "#FEE5A7", "#FEFAC2",
+ "#FFFFFF", "#E0F2FF", "#C2DBFF", "#A7C5FF", "#84A4FF", "#617EFF", "#3C55FF", "#1828FF"];
+colorScales.blackWhite = ["black", "white"];
+colorScales.puOr11 = ['#7f3b08', '#b35806', '#e08214', '#fdb863', '#fee0b6', '#f7f7f7', '#d8daeb', '#b2abd2', '#8073ac', '#542788', '#2d004b'];
+colorScales.brownBlue = ["#8c510a", "#d8b365", "#f6e8c3", "#f5f5f5", "#c7eae5", "#5ab4ac", "#01665e"];
+ colorScales.redToBlue = ["#1828FF","#3C55FF","#617EFF","#84A4FF","#A7C5FF","#C2DBFF","#E0F2FF","#FFFFFF","#FEFAC2","#FEE5A7","#FEBF79","#FE9256","#BD4A29","#BD2918", "#BD0C0C"];
+
+
+
+ // helpful Tools:
+
+ // https://color.adobe.com/create
+ // https://bl.ocks.org/starcalibre/6cccfa843ed254aa0a0d
