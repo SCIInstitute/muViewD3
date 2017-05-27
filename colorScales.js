8091b8a01f22c825e3f7df8c249c0f015774dc4b
Magdalena <a1209910@unet.univie.ac.at>
2017-05-27 14:24:28 +0200

    block also if data is there to show change

--- colorScales.js
+++ colorScales.js
@@ -12,3 +12,20 @@ colorScales.brownBlue = ["#8c510a", "#d8b365", "#f6e8c3", "#f5f5f5", "#c7eae5", 
 
  // https://color.adobe.com/create
  // https://bl.ocks.org/starcalibre/6cccfa843ed254aa0a0d
+
+
+ //from http://colorbrewer2.org/#type=diverging&scheme=BrBG&n=11
+colorScales.BrBG = ['#543005','#8c510a','#bf812d','#dfc27d','#f6e8c3','#f5f5f5','#c7eae5','#80cdc1','#35978f','#01665e','#003c30'];
+colorScales.PiYG = ['#8e0152','#c51b7d','#de77ae','#f1b6da','#fde0ef','#f7f7f7','#e6f5d0','#b8e186','#7fbc41','#4d9221','#276419'];
+colorScales.PRGn = ['#40004b','#762a83','#9970ab','#c2a5cf','#e7d4e8','#f7f7f7','#d9f0d3','#a6dba0','#5aae61','#1b7837','#00441b'];
+colorScales.PuOr = ['#7f3b08','#b35806','#e08214','#fdb863','#fee0b6','#f7f7f7','#d8daeb','#b2abd2','#8073ac','#542788','#2d004b'];
+colorScales.RdBu = ['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061'];
+colorScales.RdGy = ['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#ffffff','#e0e0e0','#bababa','#878787','#4d4d4d','#1a1a1a'];
+colorScales.RdYlBu = ['#a50026','#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4','#313695'];
+colorScales.RdYlGn = ['#a50026','#d73027','#f46d43','#fdae61','#fee08b','#ffffbf','#d9ef8b','#a6d96a','#66bd63','#1a9850','#006837'];
+colorScales.Spectral = ['#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','#e6f598','#abdda4','#66c2a5','#3288bd','#5e4fa2'];
+
+/*
+TODO:
+function to invert color scale
+*/
