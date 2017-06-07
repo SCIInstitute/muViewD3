a2b22d73f511d75de9550ccc4452333089c1db13
Magdalena <a1209910@unet.univie.ac.at>
2017-06-07 17:50:06 +0200

    finally a color legend and histogram equalized color scale

--- colorScales.js
+++ colorScales.js
@@ -2,13 +2,9 @@ var colorScales = {};
 // TODO select good default
 
 colorScales.brownBlue = ["#8c510a", "#d8b365", "#f6e8c3", "#f5f5f5", "#c7eae5", "#5ab4ac", "#01665e"]; 
-
-colorScales.brownBlueModif = ["#8c510a","#8c510a","#8c510a","#8c510a","#8c510a",     "#d8b365","#d8b365","#d8b365","#d8b365",     "#f6e8c3","#f6e8c3",     "#f5f5f5",     "#c7eae5","#c7eae5",          "#5ab4ac", "#5ab4ac","#5ab4ac", "#5ab4ac",        "#01665e", "#01665e", "#01665e", "#01665e", "#01665e"]; 
-
-colorScales.brownBlue = ["#8c510a", "#d8b365", "#f6e8c3", "#f5f5f5", "#c7eae5", "#5ab4ac", "#01665e"]; 
 colorScales.redToBlueFlip = ["#BD0C0C", "#BD2918", "#BD4A29", "#FE9256", "#FEBF79", "#FEE5A7", "#FEFAC2",
  "#FFFFFF", "#E0F2FF", "#C2DBFF", "#A7C5FF", "#84A4FF", "#617EFF", "#3C55FF", "#1828FF"];
-colorScales.blackWhite = ["black", "white"];
+colorScales.blackWhite = ['#f7f7f7','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525'];
 colorScales.puOr11 = ['#7f3b08', '#b35806', '#e08214', '#fdb863', '#fee0b6', '#f7f7f7', '#d8daeb', '#b2abd2', '#8073ac', '#542788', '#2d004b'];
 colorScales.redToBlue = ["#1828FF","#3C55FF","#617EFF","#84A4FF","#A7C5FF","#C2DBFF","#E0F2FF","#FFFFFF","#FEFAC2","#FEE5A7","#FEBF79","#FE9256","#BD4A29","#BD2918", "#BD0C0C"];
 
